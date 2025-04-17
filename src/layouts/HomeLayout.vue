0641c4cc201be46d37f3a36e20f8840ca46225a5
Sultan Evgenii <esultan@saber.games>
2025-04-17 18:08:39 +0300

    add cards

--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -1,6 +1,84 @@
 <template>
-  <h1>Home</h1>
+  <div>
+    <div class="grid grid-cols-12 gap-4">
+      <div class="col-span-12 md:col-span-6 lg:col-span-3">
+        <div class="rounded-xl p-4 shadow-md">
+          <div class="mb-4 flex justify-between">
+            <div>
+              <span class="mb-4 block font-medium">Orders</span>
+              <div class="!text-xl font-medium">152</div>
+            </div>
+            <div
+              class="flex h-10 w-10 items-center justify-center rounded-md bg-blue-100"
+            >
+              <i class="pi pi-shopping-cart !text-xl text-blue-500" />
+            </div>
+          </div>
+          <span class="font-medium text-green-500">24 new </span>
+          <span class="text-surface-500 dark:text-surface-300"
+            >since last visit</span
+          >
+        </div>
+      </div>
+      <div class="col-span-12 md:col-span-6 lg:col-span-3">
+        <div class="rounded-xl p-4 shadow-md">
+          <div class="mb-4 flex justify-between">
+            <div>
+              <span class="mb-4 block font-medium">Revenue</span>
+              <div class="!text-xl font-medium">$2.100</div>
+            </div>
+            <div
+              class="flex h-10 w-10 items-center justify-center rounded-md bg-orange-100"
+            >
+              <i class="pi pi-map-marker !text-xl text-orange-500" />
+            </div>
+          </div>
+          <span class="font-medium text-green-500">%52+ </span>
+          <span class="text-surface-500 dark:text-surface-300"
+            >since last week</span
+          >
+        </div>
+      </div>
+      <div class="col-span-12 md:col-span-6 lg:col-span-3">
+        <div class="rounded-xl p-4 shadow-md">
+          <div class="mb-4 flex justify-between">
+            <div>
+              <span class="mb-4 block font-medium">Customers</span>
+              <div class="!text-xl font-medium">28441</div>
+            </div>
+            <div
+              class="flex h-10 w-10 items-center justify-center rounded-md bg-cyan-100"
+            >
+              <i class="pi pi-inbox !text-xl text-cyan-500" />
+            </div>
+          </div>
+          <span class="font-medium text-green-500">520 </span>
+          <span class="text-surface-500 dark:text-surface-300"
+            >since last visit</span
+          >
+        </div>
+      </div>
+      <div class="col-span-12 md:col-span-6 lg:col-span-3">
+        <div class="rounded-xl p-4 shadow-md">
+          <div class="mb-4 flex justify-between">
+            <div>
+              <span class="mb-4 block font-medium">Comments</span>
+              <div class="!text-xl font-medium">152 Unread</div>
+            </div>
+            <div
+              class="flex h-10 w-10 items-center justify-center rounded-md bg-purple-100"
+            >
+              <i class="pi pi-comment !text-xl text-purple-500" />
+            </div>
+          </div>
+          <span class="font-medium text-green-500">85 </span>
+          <span class="text-surface-500 dark:text-surface-300">responded</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<script setup></script>
 
 <script setup></script>
 
